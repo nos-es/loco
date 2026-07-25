cf551ee27ebcd81bbf1e6641aa26f2f7ae4b36ea
nos-es <ethemsahbaz@outlook.com>
2026-07-25 17:26:08 +0200

    validate torrent file argument

--- src/main.c
+++ src/main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
-int main(void) {
-  puts("loco");
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    fprintf(stderr, "Usage: %s <torrent-file>\n", argv[0]);
+    return 1;
+  }
+  printf("Provided argument: %s\n", argv[1]);
   return 0;
 }
