a4ae4341c7d1d0607d1cfed1149f105d191b6733
nos-es <ethemsahbaz@outlook.com>
2026-07-26 08:55:01 +0200

    check torrent extension

--- src/cli.c
+++ src/cli.c
@@ -1,13 +1,28 @@
 #include <stdbool.h>
-#include <stdio.h>
+#include <string.h>
 
-bool cli_arguments_valid(int argc, char *argv[], const char **out_torrent_path) {
+bool has_torrent_extension(char *torrent_path) {
+
+  const char *dot = strchr(torrent_path, '.');
+
+  if (!dot || dot == torrent_path) {
+    return false;
+  }
+  return strcmp(dot, ".torrent") == 0;
+}
+
+bool cli_arguments_valid(int argc, char *argv[],
+                         const char **out_torrent_path) {
 
   if (out_torrent_path == NULL) {
     return false;
   }
 
   if (argc != 2) {
+    return false;
+  }
+
+  if (!has_torrent_extension(argv[1])) {
     return false;
   }
 
