db3f7ea318438aab37940f2a1e45361632cdd4e7
nos-es <ethemsahbaz@outlook.com>
2026-07-26 09:21:58 +0200

    refactor: remove torrent filename extension check

--- src/cli.c
+++ src/cli.c
@@ -1,19 +1,7 @@
 #include <stdbool.h>
-#include <string.h>
+#include <stdio.h>
 
-bool has_torrent_extension(char *torrent_path) {
-
-  const char *dot = strchr(torrent_path, '.');
-
-  if (!dot || dot == torrent_path) {
-    return false;
-  }
-  return strcmp(dot, ".torrent") == 0;
-}
-
-bool cli_arguments_valid(int argc, char *argv[],
-                         const char **out_torrent_path) {
-
+bool cli_arguments_valid(int argc, char *argv[], const char **out_torrent_path) {
   if (out_torrent_path == NULL) {
     return false;
   }
@@ -22,11 +10,6 @@ bool cli_arguments_valid(int argc, char *argv[],
     return false;
   }
 
-  if (!has_torrent_extension(argv[1])) {
-    return false;
-  }
-
   *out_torrent_path = argv[1];
-
   return true;
 }
