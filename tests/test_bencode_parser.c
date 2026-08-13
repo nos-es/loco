36631fbf00da1e19a6aa8854c5bf42df38ebd725
nos-es <ethemsahbaz@outlook.com>
2026-08-13 11:48:20 +0200

    test parse bencode list with single element

--- tests/test_bencode_parser.c
+++ tests/test_bencode_parser.c
@@ -479,7 +479,8 @@ test_parse_bencode_buffer_parses_empty_list(const MunitParameter params[],
   munit_assert_true(parser_initialzed);
 
   bencode_object_t parsed_obj = {
-      .type = INVALID, .value.list = {.items = NULL, .count = 99, .capacity = 123}};
+      .type = INVALID,
+      .value.list = {.items = NULL, .count = 99, .capacity = 123}};
 
   bool parsed = parse_bencode_buffer(&parser, &parsed_obj);
 
@@ -489,6 +490,39 @@ test_parse_bencode_buffer_parses_empty_list(const MunitParameter params[],
   munit_assert_ptr(parsed_obj.value.list.items, ==, NULL);
   munit_assert_size(parsed_obj.value.list.capacity, ==, 0);
   munit_assert_size(parsed_obj.value.list.count, ==, 0);
+
+  return MUNIT_OK;
+}
+static MunitResult test_parse_bencode_buffer_parses_single_integer_in_list(
+    const MunitParameter params[], void *user_data) {
+  (void)params;
+  (void)user_data;
+
+  parser_state_t parser;
+  const unsigned char input_data[] = "li42ee";
+  size_t input_length = sizeof(input_data) - 1;
+  size_t expected_parser_position_after_parse = sizeof(input_data) - 1;
+
+  bool parser_initialzed =
+      bencode_parser_init(&parser, input_data, input_length);
+
+  munit_assert_true(parser_initialzed);
+
+  bencode_object_t parsed_obj = {
+      .type = INVALID,
+      .value.list = {.items = NULL, .count = 99, .capacity = 123}};
+
+  bool parsed = parse_bencode_buffer(&parser, &parsed_obj);
+
+  munit_assert_true(parsed);
+  munit_assert_size(parser.position, ==, expected_parser_position_after_parse);
+  munit_assert_int(parsed_obj.type, ==, LIST);
+  munit_assert_ptr(parsed_obj.value.list.items, !=, NULL);
+  munit_assert_size(parsed_obj.value.list.capacity, ==, 1);
+  munit_assert_size(parsed_obj.value.list.count, ==, 1);
+  munit_assert_int(parsed_obj.value.list.items[0].type, ==, INTEGER);
+  munit_assert_int(parsed_obj.value.list.items[0].value.integer, ==, 42);
+  free(parsed_obj.value.list.items);
 
   return MUNIT_OK;
 }
@@ -550,6 +584,9 @@ static MunitTest tests[] = {
      MUNIT_TEST_OPTION_NONE, NULL},
     {"/parse/lists/parse_empty_list",
      test_parse_bencode_buffer_parses_empty_list, NULL, NULL,
+     MUNIT_TEST_OPTION_NONE, NULL},
+    {"/parse/lists/single_integer_in_list",
+     test_parse_bencode_buffer_parses_single_integer_in_list, NULL, NULL,
      MUNIT_TEST_OPTION_NONE, NULL},
     {NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}
 
