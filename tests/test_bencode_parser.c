9260fa04bd4d345ce9631b9c2547a6118c3e9fdf
nos-es <ethemsahbaz@outlook.com>
2026-08-16 08:51:18 +0200

    test for parsing empty dictionary

--- tests/test_bencode_parser.c
+++ tests/test_bencode_parser.c
@@ -743,6 +743,41 @@ test_free_bencode_buffer_reject_partially_parsed_list_and_cleans_ups(
   return MUNIT_OK;
 }
 
+static MunitResult
+test_bencode_buffer_parses_empty_dictionary(const MunitParameter params[],
+                                                 void *user_data) {
+  (void)params;
+  (void)user_data;
+
+  parser_state_t parser;
+  const unsigned char input_data[] = "de";
+  size_t input_length = sizeof(input_data) - 1;
+  size_t expected_parser_position_after_parse = sizeof(input_data) - 1;
+
+  bool parser_initialzed =
+      bencode_parser_init(&parser, input_data, input_length);
+
+  munit_assert_true(parser_initialzed);
+
+  size_t dummy_count = 99;
+  size_t dummy_capacity = 123;
+  bencode_object_t parsed_obj = {
+      .type = INVALID,
+      .value.dictionary = {
+          .entries = NULL, .count = dummy_count, .capacity = dummy_capacity}};
+
+  bool parsed = parse_bencode_buffer(&parser, &parsed_obj);
+
+  munit_assert_true(parsed);
+  munit_assert_size(parser.position, ==, expected_parser_position_after_parse);
+  munit_assert_int(parsed_obj.type, ==, DICTIONARY);
+  munit_assert_size(parsed_obj.value.dictionary.count, ==, 0);
+  munit_assert_ptr(parsed_obj.value.dictionary.entries, ==, NULL);
+  munit_assert_size(parsed_obj.value.dictionary.capacity, ==, 0);
+
+  return MUNIT_OK;
+}
+
 static MunitTest tests[] = {
     {"/init/accepts-valid-buffer",
      test_bencode_parser_init_accepts_valid_buffer, NULL, NULL,
@@ -819,6 +854,9 @@ static MunitTest tests[] = {
     {"/parse/lists/rejects_partially_parsed_lists",
      test_free_bencode_buffer_reject_partially_parsed_list_and_cleans_ups, NULL,
      NULL, MUNIT_TEST_OPTION_NONE, NULL},
+    {"/parse/dictionaries/parses-empty-dictionary",
+     test_bencode_buffer_parses_empty_dictionary, NULL, NULL,
+     MUNIT_TEST_OPTION_NONE, NULL},
     {NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}
 
 };
