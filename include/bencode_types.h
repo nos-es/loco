41622f22fb6f0a990f36ed5edc8b3c76c465b055
nos-es <ethemsahbaz@outlook.com>
2026-08-16 08:41:09 +0200

    add dictionary structs

--- include/bencode_types.h
+++ include/bencode_types.h
@@ -3,6 +3,7 @@
 #include <stdint.h>
 
 typedef struct BencodeObject bencode_object_t;
+typedef struct BencodeDictionaryEntry bencode_dictionary_entry_t;
 
 typedef struct BencodeSegment {
   const unsigned char *data;
@@ -24,13 +25,25 @@ typedef enum BencodeDataType {
   INVALID
 } bencode_data_type_t;
 
+typedef struct BencodeDictionary {
+  bencode_dictionary_entry_t *entries;
+  size_t capacity;
+  size_t count;
+} bencode_dictionary_t;
+
 typedef union BencodeValue {
   int64_t integer;
   bencode_segment_t byte_string;
   bencode_list_t list;
+  bencode_dictionary_t dictionary;
 } bencode_value_t;
 
 typedef struct BencodeObject {
   bencode_data_type_t type;
   bencode_value_t value;
 } bencode_object_t;
+
+typedef struct BencodeDictionaryEntry {
+  bencode_segment_t key;
+  bencode_object_t value;
+} bencode_dictionary_entry_t;
