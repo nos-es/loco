dc90c07fc82690fa1c8fef0a6f6aecaf97c7a3f8
nos-es <ethemsahbaz@outlook.com>
2026-08-19 13:31:35 +0200

    add find name key function and refactor key search into helper function

--- include/torrent_metadata.h
+++ include/torrent_metadata.h
@@ -3,3 +3,6 @@
 
 const bencode_object_t *
 torrent_metadata_find_info(const bencode_object_t *root);
+
+const bencode_object_t *
+torrent_metadata_find_name(const bencode_object_t *info);
--- src/torrent_metadata.c
+++ src/torrent_metadata.c
@@ -3,38 +3,53 @@
 #include <stddef.h>
 #include <string.h>
 
-static const unsigned char info_key_name[] = "info";
-static const size_t info_key_name_len = sizeof(info_key_name) - 1;
-
-const bencode_object_t *
-torrent_metadata_find_info(const bencode_object_t *root) {
+static const bencode_object_t *
+find_entry_in_dictionary(const bencode_object_t *root,
+                         const unsigned char *key_name, size_t key_length,
+                         bencode_data_type_t target_type) {
   if (root == NULL) {
     return NULL;
   }
   if (root->type != DICTIONARY) {
     return NULL;
   }
-
   for (size_t i = 0; i < root->value.dictionary.count; i++) {
 
     const bencode_dictionary_entry_t *current_entry =
         &root->value.dictionary.entries[i];
 
-    if (current_entry->key.length != info_key_name_len) {
+    if (current_entry->key.length != key_length) {
       continue;
     }
 
-    int compare_result = memcmp(current_entry->key.data, info_key_name,
-                                current_entry->key.length);
+    int compare_result =
+        memcmp(current_entry->key.data, key_name, current_entry->key.length);
     if (compare_result != 0) {
       continue;
     }
 
-    if (current_entry->value.type != DICTIONARY) {
+    if (current_entry->value.type != target_type) {
       continue;
     }
 
     return &current_entry->value;
   }
   return NULL;
+}
+const bencode_object_t *
+torrent_metadata_find_info(const bencode_object_t *root) {
+
+  const unsigned char info_key_name[] = "info";
+  const size_t key_len = sizeof(info_key_name) - 1;
+
+  return find_entry_in_dictionary(root, info_key_name, key_len, DICTIONARY);
+}
+
+const bencode_object_t *
+torrent_metadata_find_name(const bencode_object_t *info) {
+
+  static const unsigned char name_key_name[] = "name";
+  const size_t key_len = sizeof(name_key_name) - 1;
+
+  return find_entry_in_dictionary(info, name_key_name, key_len, BYTE_STRING);
 }
