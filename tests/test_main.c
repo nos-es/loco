c1438329e2d1f0ac9f5eaa0920372342fce176c4
nos-es <ethemsahbaz@outlook.com>
2026-08-28 07:47:07 +0200

    added info hash test suite to main

--- tests/test_main.c
+++ tests/test_main.c
@@ -2,18 +2,20 @@
 
 extern const MunitSuite bencode_parser_suite;
 extern const MunitSuite torrent_metadata_suite;
+extern const MunitSuite info_hash_suite;
 
 int main(int argc, char *argv[]) {
 
   MunitSuite sub_suite[] = {bencode_parser_suite,
                             torrent_metadata_suite,
+                            info_hash_suite,
                             {NULL, NULL, NULL, 0, MUNIT_SUITE_OPTION_NONE}};
 
   const MunitSuite root_suite = {.prefix = "",
-                                .tests = NULL,
-                                .suites = sub_suite,
-                                .iterations = 1,
-                                .options = MUNIT_SUITE_OPTION_NONE};
+                                 .tests = NULL,
+                                 .suites = sub_suite,
+                                 .iterations = 1,
+                                 .options = MUNIT_SUITE_OPTION_NONE};
 
   return munit_suite_main(&root_suite, NULL, argc, argv);
 }
