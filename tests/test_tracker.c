86c1779c349e8bbfecdbb2f6b99b2a10425640c0
nos-es <ethemsahbaz@outlook.com>
2026-08-31 08:10:26 +0200

    tests build tracker url

--- tests/test_tracker.c
+++ tests/test_tracker.c
@@ -3,9 +3,12 @@
 #include "tracker.h"
 #include <stddef.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 
-static MunitResult test_build_tracker_url_returns_correct_announce_url(
-    const MunitParameter params[], void *user_data) {
+static MunitResult
+test_build_tracker_url_returns_correct_url(const MunitParameter params[],
+                                           void *user_data) {
   (void)params;
   (void)user_data;
   const unsigned char announce_bytes[] = "https://tracker.example/announce";
@@ -25,18 +28,64 @@ static MunitResult test_build_tracker_url_returns_correct_announce_url(
       .left = 100,
       .compact = true};
 
+  const char *expected_url =
+      "https://tracker.example/"
+      "announce?info_hash=%00%01%20%25%2F%3FAZaz%7F%80%A9%FF%"
+      "10-_.~0&peer_id=-LO0001-%00%01%20%25%2F%3FAz%80%A9%FF_&"
+      "port=1234&uploaded=0&downloaded=0&left=100&compact=1";
+
   char *result = build_tracker_url(&announce, &tracker_req);
   munit_assert_not_null(result);
-  munit_assert_memory_equal(announce.length, announce.data, result);
+  munit_assert_string_equal(expected_url, result);
+  free(result);
+
+  return MUNIT_OK;
+}
+
+static MunitResult
+test_build_tracker_url_returns_correct_url_with_questionmark_in_announce(
+    const MunitParameter params[], void *user_data) {
+  (void)params;
+  (void)user_data;
+  const unsigned char announce_bytes[] =
+      "https://tracker.example/announce?token=abc";
+  bencode_segment_t announce = {.data = announce_bytes,
+                                .length = sizeof(announce_bytes) - 1};
+
+  tracker_request_t tracker_req = {
+      .info_hash = {.bytes = {0x00, 0x01, 0x20, 0x25, 0x2f, 0x3f, 0x41,
+                              0x5a, 0x61, 0x7a, 0x7f, 0x80, 0xa9, 0xff,
+                              0x10, 0x2d, 0x5f, 0x2e, 0x7e, 0x30}},
+      .peer_id = {.bytes = {'-',  'L',  'O',  '0',  '0',  '0',  '1',
+                            '-',  0x00, 0x01, 0x20, 0x25, 0x2f, 0x3f,
+                            0x41, 0x7a, 0x80, 0xa9, 0xff, 0x5f}},
+      .port = 1234,
+      .uploaded = 0,
+      .downloaded = 0,
+      .left = 100,
+      .compact = true};
+
+  const char *expected_url =
+      "https://tracker.example/"
+      "announce?token=abc&info_hash=%00%01%20%25%2F%3FAZaz%7F%80%A9%FF%"
+      "10-_.~0&peer_id=-LO0001-%00%01%20%25%2F%3FAz%80%A9%FF_&"
+      "port=1234&uploaded=0&downloaded=0&left=100&compact=1";
+
+  char *result = build_tracker_url(&announce, &tracker_req);
+  munit_assert_not_null(result);
+  munit_assert_string_equal(expected_url, result);
   free(result);
 
   return MUNIT_OK;
 }
 
 static MunitTest tests[] = {
-    {"/build_tracker_url/returns-correct-announce",
-     test_build_tracker_url_returns_correct_announce_url, NULL, NULL,
+    {"/build_tracker_url/returns-correct-url",
+     test_build_tracker_url_returns_correct_url, NULL, NULL,
      MUNIT_TEST_OPTION_NONE, NULL},
+    {"/build_tracker_url/returns-correct-url-questionmark-announce",
+     test_build_tracker_url_returns_correct_url_with_questionmark_in_announce,
+     NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL},
     {NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}
 
 };
