015e9ef49d3f93e52a2e0409973e07729c8e3bbd
nos-es <ethemsahbaz@outlook.com>
2026-09-10 11:01:32 +0200

    add peer connection module

---
+++ include/peer_connection.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "tracker.h"
+#include <stdbool.h>
+
+bool connect_to_peer(peer_t *peer);
---
+++ src/peer_connection.c
@@ -0,0 +1,2 @@
+#include "tracker.h"
+bool connect_to_peer(peer_t *peer) { return false; }
