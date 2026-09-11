8eef6db1058aa205ee178234dcc874b7c4aa31ea
nos-es <ethemsahbaz@outlook.com>
2026-09-11 10:47:09 +0200

    Update header file with constants and handshake function signatures

--- include/peer_connection.h
+++ include/peer_connection.h
@@ -1,4 +1,14 @@
 #pragma once
+#include "info_hash.h"
+#include "peer_id.h"
 #include "tracker.h"
 
-int connect_to_peer(peer_t *peer);
+enum {
+  BITTORRENT_PROTOCOL_NAME_LENGTH = 19,
+  HANDSHAKE_BYTES_LENGTH = 68,
+};
+
+int connect_to_peer(const peer_t *peer);
+
+bool handshake_with_peer(int file_descriptor, const info_hash_t *info_hash,
+                         const peer_id_t *peer_id);
