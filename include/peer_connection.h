fb7891c612673aecbfad235850b9fdcb55bce236
nos-es <ethemsahbaz@outlook.com>
2026-09-11 12:35:25 +0200

    add peer wire message struct

--- include/peer_connection.h
+++ include/peer_connection.h
@@ -2,11 +2,33 @@
 #include "info_hash.h"
 #include "peer_id.h"
 #include "tracker.h"
+#include <stdbool.h>
+#include <stddef.h>
 
 enum {
   BITTORRENT_PROTOCOL_NAME_LENGTH = 19,
   HANDSHAKE_BYTES_LENGTH = 68,
 };
+
+enum MessageId {
+  PEER_MESSAGE_CHOKE = 0,
+  PEER_MESSAGE_UNCHOKE = 1,
+  PEER_MESSAGE_INTERESTED = 2,
+  PEER_MESSAGE_NOT_INTERESTED = 3,
+  PEER_MESSAGE_HAVE = 4,
+  PEER_MESSAGE_BITFIELD = 5,
+  PEER_MESSAGE_REQUEST = 6,
+  PEER_MESSAGE_PIECE = 7,
+  PEER_MESSAGE_CANCEL = 8,
+  PEER_MESSAGE_INVALID = 9
+};
+
+typedef struct PeerWireMessage {
+  enum MessageId message_id;
+  unsigned char *payload;
+  size_t payload_length;
+  bool is_keep_alive;
+} peer_wire_message_t;
 
 int connect_to_peer(const peer_t *peer);
 
